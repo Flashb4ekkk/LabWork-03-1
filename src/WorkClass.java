2151046ebadf064ab72e184d178c8c22e8ce04eb
Bethakzs <romanwdwd@gmail.com>
2023-09-24 20:00:21 +0300

    updated commit

--- src/WorkClass.java
+++ src/WorkClass.java
@@ -5,7 +5,19 @@ public class WorkClass {
         Scanner skan = new Scanner(System.in);
         System.out.println("Input x");
         double x = skan.nextDouble();
-        double y, A = 2 * Math.abs(5 - x);
+        double y = 0, A = 2 * Math.abs(5 - x);
+
+        //спосіб 1
+        if(x <= -1){
+            y = A - Math.exp(Math.abs(2+x));
+        }
+        if(x > -1 & x < 1){
+            y = A - Math.pow(Math.sin(1 / Math.abs(2 + x)), 2);
+        }
+        if(x >= 1){
+            y = A - (Math.pow(Math.cos(x), 2)) / (1 + Math.abs(Math.sin(x)));
+        }
+        System.out.println("result of first method: " + Math.round(y*100000.0)/100000.0);
 
         //спосіб 2
         if(x <= -1){
@@ -15,6 +27,6 @@ public class WorkClass {
         }else {
             y = A - (Math.pow(Math.cos(x), 2)) / (1 + Math.abs(Math.sin(x)));
         }
-        System.out.println(y);
+        System.out.println("result of second method: " + Math.round(y*100000.0)/100000.0);;
     }
 }
